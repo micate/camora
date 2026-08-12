805d954f0339ef380a571862370acacd54b45322
micate <micate@me.com>
2026-08-12 14:43:56 +0800

    chore(build): 发布新版本1.5.0
    - 更新 package.json 中的版本号为 1.5.0
    - 更新 wxt.config.ts 配置文件的版本号为 1.5.0

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   manifest: {
     name: "Camora",
-    version: "1.4.2",
+    version: "1.5.0",
     description: "__MSG_description__",
     permissions: [
       "alarms",
