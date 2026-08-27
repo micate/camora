f6b81b4903d0fe0d17be015127f63b8af1c525e7
micate <micate@me.com>
2026-08-27 16:49:28 +0800

    fix(utils): 解决启用状态徽章文本显示问题

--- utils/updateStatus.ts
+++ utils/updateStatus.ts
@@ -3,8 +3,8 @@ export function updateStatus(enabled: boolean, count?: number) {
     chrome.action.setBadgeText({ text: count.toString() });
     chrome.action.setBadgeBackgroundColor({ color: "rgb(22, 104, 220)" });
   } else if (enabled) {
-    // 启用状态但没有规则
-    chrome.action.setBadgeText({ text: '' });
+    // 启用状态下即使没有生效规则，也明确显示为 0
+    chrome.action.setBadgeText({ text: '0' });
     chrome.action.setBadgeBackgroundColor({ color: "rgb(22, 104, 220)" });
   } else {
     chrome.action.setBadgeText({ text: "OFF" });
